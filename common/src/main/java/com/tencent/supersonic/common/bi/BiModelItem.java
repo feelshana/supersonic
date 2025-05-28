263524875d92e607c3ca53d3c4005ea6ee4afdfa
wanghaibo <wanghaibo@richinfo.cn>
2025-05-28 17:22:41 +0800

    BI创建智能助手接口

---
+++ common/src/main/java/com/tencent/supersonic/common/bi/BiModelItem.java
@@ -0,0 +1,24 @@
+package com.tencent.supersonic.common.bi;
+
+import lombok.Data;
+
+@Data
+public class BiModelItem {
+    private String id;
+    private String databaseName;
+    private String tableName;
+    private String tableAliasName;
+    private String columnName;
+    private String columnId;
+    private String columnActualName;
+    private String isLook;
+    private String format;
+    private String name;
+    private String colNum;
+    private Integer columnType;
+    private Integer originTableType;
+    private String description;
+    private String databaseTableColumnName;
+    private Integer type;
+    private String isDict;
+}
---
+++ common/src/main/java/com/tencent/supersonic/common/bi/BiModelParam.java
@@ -0,0 +1,10 @@
+package com.tencent.supersonic.common.bi;
+
+import lombok.Data;
+
+@Data
+public class BiModelParam {
+    private String id;
+    private String name;
+    private String nameAlias;
+}
