4fe7df4dd1b3654492e4afc1418268e33e58d742
阳光 <shuanqverymuch@gmail.com>
2025-08-28 15:28:44 +0800

    fix: 修改modelId的正确传参

--- webapp/packages/supersonic-fe/src/pages/Agent/AgentFormForLink.tsx
+++ webapp/packages/supersonic-fe/src/pages/Agent/AgentFormForLink.tsx
@@ -1,6 +1,7 @@
 import { AgentType, ModelType } from './type';
 import AgentForm1 from './AgentForm1';
 import { saveAgent, getAgentList, getModelList } from './service';
+import { getAllModelByDomainId } from '../../pages/SemanticModel/service';
 import { message, Spin } from 'antd';
 import { useEffect, useState } from 'react';
 import { useLocation } from 'react-router-dom';
@@ -62,8 +63,7 @@ const AgentFormForLink: React.FC = () => {
     }, [agentId]);
 
     useEffect(() => {
-      const modelId = currentAgent?.dataSetIds?.[0]
-      setModelId(modelId)
+
     }, [currentAgent])
 
     useEffect(() => {
@@ -71,7 +71,13 @@ const AgentFormForLink: React.FC = () => {
         for (let i = 0; i < domainDataSetTree.length; i++) {
           for (let j = 0; j < domainDataSetTree[i].children!.length; j++) {
             if (domainDataSetTree[i].children![j].id === modelId) {
-              setDomainId(domainDataSetTree[i].children![j].parentId)
+              const domainId = domainDataSetTree[i].children![j].parentId
+              setDomainId(domainId)
+              getAllModelByDomainId(domainId).then((res)=>{
+                if (res?.code === 200) {
+                  setModelId(res.data[0].id)
+                }
+              })
               break outer
             }
           }
